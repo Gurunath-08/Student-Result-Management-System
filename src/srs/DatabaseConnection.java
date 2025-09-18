15152bb3c780450b6fce18383872740dad5661b7
Gurumoorthy.S <157018842+Gurunath-08@users.noreply.github.com>
2025-09-18 12:56:31 +0530

    Update DatabaseConnection.java

--- src/srs/DatabaseConnection.java
+++ src/srs/DatabaseConnection.java
@@ -6,8 +6,9 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/student_db";
         String user = "root";
-        String password = "08062005"; // Or your MySQL password
+        String password = "08062005"; 
 
         return DriverManager.getConnection(url, user, password);
     }
 }
+
